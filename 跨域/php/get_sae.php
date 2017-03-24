9907462b00957415198a85142f04edb88df2bfe5
YBjavascript <446978329@qq.com>
2017-03-24 22:48:15 +0800

    Signed-off-by: YBjavascript <446978329@qq.com>

---
+++ 跨域/php/get_sae.php
@@ -0,0 +1,5 @@
+<?php
+
+$content = file_get_contents('http://192.168.2.138/2017-11-12/php/test.php');
+echo $content;
+
---
+++ 跨域/php/test.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+header('content-type:text/html;charset=utf-8');
+
+echo "成功";
+
+
+?>
